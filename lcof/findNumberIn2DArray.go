149c75b3337fa637332dc839c90627fcefa62071
zhoujianxuan <zhoujianxuan@sofish.com>
2022-08-04 09:15:18 +0800

    add lcof

---
+++ lcof/findNumberIn2DArray.go
@@ -0,0 +1,20 @@
+package lcof
+
+// 本题与主站 240 题相同：https://leetcode.cn/problems/search-a-2d-matrix-ii/
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	m, n := len(matrix), len(matrix[0])
+	for i, j := 0, n-1; i < m && j >= 0; {
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] < target {
+			i++
+		} else {
+			j--
+		}
+	}
+	return false
+}
